20f92c86d5af69e6fb65db6f72a2dfaa4d5c7027
margic <pmcrofts@margic.com>
2017-11-17 16:38:50 -0700

    adding second service and cleanup

---
+++ service2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+func main() {
+	// a call to viper to test build with viper
+	viper.AutomaticEnv()
+	flag.Parse()
+	logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Printf("Unable to create logger: %s\n", err)
+		os.Exit(1)
+	}
+	logger.Info("Started Service 2")
+}
