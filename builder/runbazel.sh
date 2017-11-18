a634a70ebef59a1e430ed377f7f4b0e3cce04cc1
margic <pmcrofts@margic.com>
2017-11-18 09:37:55 -0700

    service 1 and 2 build and push using builder image and bazelcmd file

--- builder/runbazel.sh
+++ builder/runbazel.sh
@@ -2,17 +2,17 @@
 
 . /opt/gobazel/addpass.sh
 
-# on start bazel check if there is a buildcmd file in the /source volume
+# on start bazel check if there is a bazelcmd file in the /source volume
 # doing this so it's possible to override the command running in the container
 # allowin multiple reuse of a container to run the bazel command
 # This allows us to take advantage of bazel caching rather than running a new
-# container each time. If buildcmd file is present bazel will run with that
+# container each time. If bazelcmd file is present bazel will run with that
 # command else it will use the command from the command line args
-if [ -e /source/buildcmd ] ; then
-    echo "/source/buildcmd found running bazel $(cat /source/buildcmd)"
-    bazel `cat /source/buildcmd` 
+if [ -e /source/bazelcmd ] ; then
+    echo "/source/buildcmd found running bazel $(cat /source/bazelcmd)"
+    bazel `cat /source/bazelcmd` 
 else
-    echo "/source/buildcmd not found running bazel $($@)"
+    echo "/source/bazelcmd not found running bazel $($@)"
     bazel "$@"
 fi
 
