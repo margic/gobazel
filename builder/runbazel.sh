2da70da17aee8bb75396b979ba94b0465589621e
margic <pmcrofts@margic.com>
2017-12-01 07:51:05 -0700

    Changed to pipe command to builder

--- builder/runbazel.sh
+++ builder/runbazel.sh
@@ -1,26 +1,14 @@
 #!/bin/sh
 
+# TODO THIS DOESN'T DO WHAT I INTENDED
+# see https://github.com/margic/gobazel/issues/1
 . /opt/gobazel/addpass.sh
 
 
-# was going to use this to copy .kube to ~ but seems it works from source folder
-# if [ ! -d "/root/.kube" ]; then
-#     if [ -d "/source/.kube"]; then
-#         cp -r /source/.kube /root/.kube
-#     fi
-# fi
-
-# on start bazel check if there is a bazelcmd file in the /source volume
-# doing this so it's possible to override the command running in the container
-# allowin multiple reuse of a container to run the bazel command
-# This allows us to take advantage of bazel caching rather than running a new
-# container each time. If bazelcmd file is present bazel will run with that
-# command else it will use the command from the command line args
-if [ -e /source/bazelcmd ] ; then
-    echo "/source/buildcmd found running bazel $(cat /source/bazelcmd)"
-    bazel `cat /source/bazelcmd` 
-else
-    echo "/source/bazelcmd not found running bazel $($@)"
-    bazel "$@"
-fi
+# to make it easier to reuse the container and therefore the bazel cache
+# this script will read the bazel command from stdin and execute the bazel
+# command with that
+read line
+echo "command: $line"
+bazel $line
 
