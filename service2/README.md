93a21ad28f456f88d4f5022456a64f9de2174886
margic <pmcrofts@margic.com>
2017-12-13 08:28:25 -0700

    Gokitting service 2

---
+++ service2/README.md
@@ -0,0 +1,2 @@
+Service 2 is a gokit service that I intend to convert to protobuf endpoint
+to test the protobuf compile of bazel
--- service2/main.go
+++ service2/main.go
@@ -1,10 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
 )
 
 func main() {
-	// a call to viper to test build with viper
-	fmt.Println("Test 3")
+	svc := greetingService{}
+
+	greetingHandler := httptransport.NewServer(
+		makeGreetingEndpoint(svc),
+		decodeGreetingRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/greeting", greetingHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func decodeGreetingRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	request := greetingRequest{}
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+// GreetingService returns a greeting
+type GreetingService interface {
+	Greeting(context.Context) (string, error)
+}
+
+// greetingService a type that implementes GreetingService
+type greetingService struct{}
+
+func (greetingService) Greeting(_ context.Context) (string, error) {
+	return "hello " + time.Now().String(), nil
+}
+
+// greetingRequest is empty as there are no params for request but type required
+type greetingRequest struct {
+}
+
+// greetingResponse contains the response
+type greetingResponse struct {
+	Greeting string `json:"greeting"`
+	Err      string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
+}
+
+// makeGreetingEndpoint wrap service with endpoint
+func makeGreetingEndpoint(svc GreetingService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		//req := request.(greetingRequest) ignore request but it would normally be here
+		v, err := svc.Greeting(ctx)
+		if err != nil {
+			return greetingResponse{v, err.Error()}, nil
+		}
+		return greetingResponse{v, ""}, nil
+	}
 }
